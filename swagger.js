e76b4380e3f330df7594abe0d6639463222ff675
OmChauhan16 <omc5246@gmail.com>
2025-05-08 00:06:55 +0530

    swagger implemented

---
+++ swagger.js
@@ -0,0 +1,23 @@
+const swaggerJSDoc = require('swagger-jsdoc');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Your API Title',
+      version: '1.0.0',
+      description: 'API Documentation using Swagger',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+        description: 'Development server',
+      },
+    ],
+  },
+  apis: ['./routes/*.js'], 
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+module.exports = swaggerSpec;
